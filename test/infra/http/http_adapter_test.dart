9575ae9d0be4a7d1b5ed7994a450d52f16228dc2
Breno Lopes de Souza <breno_lopes_10@yahoo.com.br>
2022-01-30 01:48:59 -0300

    ensure HttpAdapter return null if post returns 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -23,7 +23,7 @@ class HttpAdapter implements HttpClient {
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response =
         await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -81,5 +81,13 @@ void main() {
     final response = await sut.request(url: url, method: 'post');
 
     expect(response, {'any_key': 'any_value'});
+  });
+
+  test('Should return data if post returns 200 with no data', () async {
+    when(() => client.post(any(), headers: any(named: 'headers')))
+        .thenAnswer((_) async => Response('{}', 200));
+
+    final response = await sut.request(url: url, method: 'post');
+    expect(response, {});
   });
 }
